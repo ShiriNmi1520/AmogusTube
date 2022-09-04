d3a02afdfc7a3098d47167c4f064799c80daa8d2
Y.C.Huang <s20026352@gmail.com>
2022-09-04 21:15:44 +0800

    feat: NProgress Add NProgress package

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,30 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "nprogress/nprogress.css";
+import NProgress from "nprogress"
+import type {AppProps} from 'next/app'
+import {useEffect} from "react";
+import {Router} from "next/router";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function _app({Component, pageProps}: AppProps) {
+  useEffect(() => {
+    const handleRouteStart = () => {
+      NProgress.start();
+    }
+    const handleRouteDone = () => {
+      NProgress.done();
+    }
+
+    Router.events.on("routeChangeStart", handleRouteStart);
+    Router.events.on("routeChangeComplete", handleRouteDone);
+    Router.events.on("routeChangeError", handleRouteDone);
+
+    return () => {
+      Router.events.off("routeChangeStart", handleRouteStart);
+      Router.events.off("routeChangeComplete", handleRouteDone);
+      Router.events.off("routeChangeError", handleRouteDone);
+    }
+  }, [])
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default _app
